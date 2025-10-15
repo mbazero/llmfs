eda76c25be43af16f6cbca1fc08670d3e5e83cbb
Michael Adams <madams2419@gmail.com>
2025-10-15 12:01:25 -0400

    Complete section 3.4

--- python/chapter_03.py
+++ python/chapter_03.py
@@ -7,6 +7,7 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
+    from pprint import pprint as pp
     return (mo,)
 
 
@@ -14,12 +15,18 @@ def _():
 def _(mo):
     mo.md(
         r"""
-    # Section 3.3
+    # 3.3 Self-attention without trainable weights
     - Attention scores computed as dot product of query tensor and input tensors
     - Dot product is a measure of similarity--it measures how closely two tensors are aligned
+      - So attention in this simple example is just based on token embedding similarity
     - Attention scores are normalized into attention weights via softmax
+      - Softmax takes list of real numbers and converts them to probabilities that sum to one
+      - Larger values are exponentially amplified and smaller values are suppressed
+      - Called softmax because it's a "softened" version of hard max, which sets the largest value to 1 and the rest to 0
+        - Hard max is not differentiable
     - Context tensor is computed by multiplying each input tensor by its attention weight and summing
-      - What is the context vector in intuitive terms?
+      - So the context vector is an attention-weighted sum of all input tensors
+      - Input tensors with higher tension will make larger contributions to the context vector value
     """
     )
     return
@@ -44,7 +51,7 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## Compute attention weights for single input vector""")
+    mo.md(r"""## Compute context vector for single input token""")
     return
 
 
@@ -90,7 +97,7 @@ def _(attn_weights_2, inputs, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## Compute attention weights for all input tokens""")
+    mo.md(r"""## Compute context vector for all input tokens""")
     return
 
 
@@ -122,6 +129,220 @@ def _(attn_weights, context_2, inputs, torch):
     context = compute_context(inputs, attn_weights)
     assert torch.allclose(context_2, context[2 - 1])
     print(context)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    # 3.4 Self-attention with trainable weights
+    - Basic self-attention with trainable weights is called *scaled dot-product attention*
+    - Introduce three trainable weight matrices $W_q$, $W_k$, and $W_v$ which represent query, key, and value matrices respectively
+    - These matrices serve to project input embeddings into query, key, and value spaces respectively
+    - Steps to compute context vector for query input $x^{(n)}$:
+      1. Compute query vector for query input only as $q^{(n)} = W_q \cdot x^{(n)}$ 
+      2. Compute key and value vectors for all inputs as:
+        - $k^{(i)} = W_k \cdot x^{(i)}$
+        - $v^{(i)} = W_v \cdot x^{(i)}$
+      3. Compute attention scores as dot product between input query vector and each key vector
+        - $\omega_{ni} = q^{(n)} \cdot k^{(i)}$
+    > Contrast this against the previous mechanism which simple computed the attention score as the dot product of the current input vector with each other input vector
+      4. Compute attention weights by scaling attention scores by the square root of the embedding dimension and then applying *softmax*
+        - $\alpha_{ni} = softmax(\omega_{ni} / \sqrt{d_k})$
+    > Why do we normalize by the embedding dimension size?
+    >
+    > The high level reason is to improve training peformance by avoiding small gradients. But why is this at the mechanical level?
+      5. Compute context vector as sum of value vectors weighted by attention score
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Compute context vector for single input token""")
+    return
+
+
+@app.cell
+def _(inputs):
+    x_2 = inputs[1]
+    d_in = inputs.shape[1]
+    d_out = 2
+
+    print(f"{x_2=}")
+    print(f"{d_in=}")
+    print(f"{d_out=}")
+    return d_in, d_out, x_2
+
+
+@app.cell
+def _(d_in, d_out, torch):
+    torch.manual_seed(123)  # For reproducible results
+
+    W_q = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+    W_k = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+    W_v = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+    print(f"{W_q=}")
+    print(f"{W_k=}")
+    print(f"{W_v=}")
+    return W_k, W_q, W_v
+
+
+@app.cell
+def _(W_k, W_q, W_v, x_2):
+    q_2 = x_2 @ W_q
+    k_2 = x_2 @ W_k
+    v_2 = x_2 @ W_v
+
+    print(f"{q_2=}")
+    print(f"{k_2=}")
+    print(f"{v_2=}")
+    return (q_2,)
+
+
+@app.cell
+def _(inputs):
+    inputs
+    return
+
+
+@app.cell
+def _(W_k, W_v, inputs):
+    keys = inputs @ W_k
+    values = inputs @ W_v
+
+    print(f"{keys=}")
+    print(f"{values=}")
+    print(f"{keys.shape=}")
+    print(f"{values.shape=}")
+    return keys, values
+
+
+@app.cell
+def _(keys, q_2):
+    sdp_attn_scores_2 = q_2 @ keys.T
+    print(f"{sdp_attn_scores_2=}")
+    return (sdp_attn_scores_2,)
+
+
+@app.cell
+def _(keys, sdp_attn_scores_2, torch):
+    embed_dim = keys.shape[-1]
+    scaled_attn_weights_2 = sdp_attn_scores_2 / embed_dim**0.5
+    sdp_attn_weights_2 = torch.softmax(scaled_attn_weights_2, dim=-1)
+    print(f"{sdp_attn_weights_2=}")
+    return (sdp_attn_weights_2,)
+
+
+@app.cell
+def _(sdp_attn_weights_2, values):
+    context_vec_2 = sdp_attn_weights_2 @ values
+    print(f"{context_vec_2=}")
+    return (context_vec_2,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Compute context vector for all input tokens""")
+    return
+
+
+@app.cell
+def _(torch):
+    import torch.nn as nn
+
+
+    class SelfAttention_v1(nn.Module):
+        def __init__(self, d_in, d_out):
+            super().__init__()
+            self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+            self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+            self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+        def forward(self, x):
+            keys = x @ self.W_key
+            queries = x @ self.W_query
+            values = x @ self.W_value
+            attn_scores = queries @ keys.T  # omega
+            attn_weights = torch.softmax(
+                attn_scores / keys.shape[-1] ** 0.5, dim=-1
+            )
+            context_vec = attn_weights @ values
+            return context_vec
+    return SelfAttention_v1, nn
+
+
+@app.cell
+def _(SelfAttention_v1, context_vec_2, d_in, d_out, inputs, torch):
+    torch.manual_seed(123)
+    sa_v1 = SelfAttention_v1(d_in, d_out)
+    context_vec = sa_v1(inputs)
+    assert torch.allclose(context_vec[1], context_vec_2)
+    print(f"{context_vec=}")
+    return
+
+
+@app.cell
+def _(nn, torch):
+    # Improvement upon v1 self-attention with nn.Linear, which implements y = M @ X + b, which is effectively a normal matrix mult when bias term is disabled. nn.Linear also automatically initializes weights with an optimized weight init scheme, leading to more stable and effective model training.
+    class SelfAttention_v2(nn.Module):
+        def __init__(self, d_in, d_out, qkv_bias=False):
+            super().__init__()
+            self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+            self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+            self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+        def forward(self, x):
+            keys = self.W_key(x)
+            queries = self.W_query(x)
+            values = self.W_value(x)
+            attn_scores = queries @ keys.T
+            attn_weights = torch.softmax(
+                attn_scores / keys.shape[-1] ** 0.5, dim=-1
+            )
+            context_vec = attn_weights @ values
+            return context_vec
+    return (SelfAttention_v2,)
+
+
+@app.cell
+def _(SelfAttention_v2, d_in, d_out, inputs, torch):
+    torch.manual_seed(789)
+    sa_v2 = SelfAttention_v2(d_in, d_out)
+    print(sa_v2(inputs))
+    return (sa_v2,)
+
+
+@app.cell
+def _(sa_v2):
+    sa_v2.W_query._parameters["weight"]
+    return
+
+
+@app.cell
+def _(SelfAttention_v1, SelfAttention_v2, d_in, d_out, inputs, nn, torch):
+    def validate():
+        """
+        Validate that SA v1 and v2 produce the same results when inited with the same weights
+        """
+        sa_v1 = SelfAttention_v1(d_in, d_out)
+        sa_v2 = SelfAttention_v2(d_in, d_out)
+
+        # Inject v2 weights to v2
+        sa_v1.W_query = nn.Parameter(sa_v2.W_query._parameters["weight"].T)
+        sa_v1.W_key = nn.Parameter(sa_v2.W_key._parameters["weight"].T)
+        sa_v1.W_value = nn.Parameter(sa_v2.W_value._parameters["weight"].T)
+
+        c_v1 = sa_v1(inputs)
+        c_v2 = sa_v2(inputs)
+
+        torch.allclose(c_v1, c_v2)
+
+
+    validate()
     return
 
 
